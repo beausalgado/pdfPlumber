1222d7028e52cac9d02b3f4311503b0e90aec263
beau-augusto <beausalgado@gmail.com>
2022-12-26 00:40:41 -0300

    v 1.0 data filter works

--- objectSecondDraft.js
+++ objectSecondDraft.js
@@ -12,71 +12,94 @@ if (myModule[i].fontname.includes("Bold")) {
 /*     remove the let to createNewEntry so that it's accessible outside the function */
      createNewEntry = {
      entry:myModule[i].text,
-/*      Create a first meaning object since all words have at least one meaning */
      meaning1: {}
     }
     /* An empty array to add meanings to */
     let newMeaningArray = []
 /* a counter to determine which meaning per word */
     let meaningCount = 1;
+    let words = [];
+    let phrases = [];
+    let americanisms = [];
+    let antonyms = [];
 /* Runs a second for loop to push synonyms to array and then attaches the array to the object */ 
 /* Starts on i plus 1 so that it doesn't include the bold word */ 
  for (let j = i+1; !myModule[j].fontname.includes("Bold") ;j++) {
 
 
 
-     /* This is to filter for the second meaning of a word. Here the text is always regular and there's a gap between 12 and 14 between the start and the word before it */
-     if((myModule[j].top - myModule[j-1].top > 12 && myModule[j].top - myModule[j-1].top < 14) && myModule[j].fontname.includes("Regular")) {
+     /* This is to filter for the second meaning of a word. Here the text is always regular and there's a gap between 12 and 14 between the start and the word before it and isn't a phrase */
+     if((myModule[j].top - myModule[j-1].top > 12 && myModule[j].top - myModule[j-1].top < 14) && (myModule[j].fontname.includes("Regular") && !/\s/.test(myModule[j].text))) {
+        /* First it adds the previous meaning to the entry object */
         meaningCount = meaningCount + 1;
-        console.log(meaningCount)
         /* adds another meaning to the entry object */
         createNewEntry["meaning" + meaningCount] = {};
-        console.log(createNewEntry)
 
-             
-             console.log("numbers game")
+/* sets the arrays equal to zero to catch another group */
+         words = [];
+         phrases = [];
+         americanisms = [];
+         antonyms = [];
         }
         
         /*    if it doesn't have spaces and has regular text, then it's a normal entry */
         if (myModule[j].fontname.includes("Regular") && !/\s/.test(myModule[j].text)){
-            
-            console.log("regular", j)
+            /* collects the words into an array */
+            words.push(myModule[j].text);
         }
+        /* Adds the array onto the object but first checks to see if it exists so that it doesn't execute it every time round*/
+        if (!createNewEntry["meaning" + meaningCount].words){
+
+            createNewEntry["meaning" + meaningCount].words = words;
+        }
+        /* clears the array for the second meaning */
+
         /* If it has spaces and doesn't have the text (amer.), then it's a phrase */
         if (/\s/.test(myModule[j].text) && !myModule[j].text.includes("(amer.)")){
-            console.log("phrase", j)
+            phrases.push(myModule[j].text);
+        }
+
+        /* Adds the array onto the object but first checks to see if it exists so that it doesn't execute it every time round*/
+        if (!createNewEntry["meaning" + meaningCount].phrases){
+
+            createNewEntry["meaning" + meaningCount].phrases = phrases;
         }
         if (myModule[j].text.includes("(amer.)")) {
-            console.log("americanismo", j)
+            americanisms.push(myModule[j].text);
+        }
+
+        /* Adds the array onto the object but first checks to see if it exists so that it doesn't execute it every time round*/
+        if (!createNewEntry["meaning" + meaningCount].americanisms){
+
+            createNewEntry["meaning" + meaningCount].americanisms = americanisms;
 
         }
+
+
         if (myModule[j].fontname.includes("Italic")) {
-            console.log("italics", j)
-
+            antonyms.push(myModule[j].text);
+            
         }
 
 
-        
-        // newMeaningArray.push(myModule[j].text);
-        
+        /* Adds the array onto the object but first checks to see if it exists so that it doesn't execute it every time round*/
+        if (!createNewEntry["meaning" + meaningCount].antonyms){
+
+
+            createNewEntry["meaning" + meaningCount].antonyms = antonyms;
+        }
     }
-    /*     Labels the meaning so as to differenciate from other meanings */
-    createNewEntry.meaning1.words = newMeaningArray;
+
     console.log(createNewEntry)
 
-
 /* stop the for loops  */
- return newMeaningArray
+ return createNewEntry
     
 }
 
-
-
-    
-
     }
    
-     
     }
-    formatTheBeautifulObject()
+
+formatTheBeautifulObject()
     
